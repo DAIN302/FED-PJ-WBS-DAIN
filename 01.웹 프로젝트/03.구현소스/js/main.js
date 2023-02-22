4b6d26c1a774172f5040865df4fe7bee0b52d97b
DAIN302 <dbgnfk12@gmail.com>
2023-02-22 23:44:10 +0900

    1차 PJ 예약 선택폼 객실/인원수 선택박스 구현
    1. 상단의 숫자를 누르면 선택 옵션박스가 나타나도록 구현, 이때 화살표 방향도 바꿔줌
    -> offsetHeight를 활용하여 ul의 높이를 구한 후 상위의 div에 해당 높이 적용
    2. 옵션 선택박스에서 누른 숫자가 적용되도록 함
    - forEach문과 click 이벤트 활용
    3. 옵션선택박스에서 숫자를 누르거나 다시 상단의 숫자를 누르면 옵션선택박스가 닫아지게함(if문 사용)

--- 01.웹 프로젝트/03.구현소스/js/main.js
+++ 01.웹 프로젝트/03.구현소스/js/main.js
@@ -6,28 +6,47 @@
     ul과 li의 디자인을 select태그처럼 함
     1. 상단의 숫자를 누르면 선택 옵션박스가 나타남
        이때 아래를 향한 화살표 아이콘이 위 방향으로 변함
-       대상1 : .main_select -> 클래스 on추가
-       대상2 : .main_select_arrow span -> trnasform : rotate(180deg) 적용
+       대상1 : .main_select -> 클릭할 대상
+       대상2 : .main_select ul -> height 값 변경
+       대상3 : .main_select_arrow span -> trnasform : rotate(180deg) 적용
     2. 옵션선택박스에서 누른 숫자가 상단의 숫자에 적용
        대상 : .main_select_value, .option
 */
 
 // 대상선정 : main_select클래스
 const mainSel = document.querySelectorAll(".main_select");
-// 화살표아이콘
-let selArrow = document.querySelectorAll(".main_select_arrow span");
 
 
-// 이벤트 : main_select클래스에 클래스 on추가
+// 상단의 숫자를 누르면 선택 옵션박스가 나타남 -> height 값 변경
 for(let x of mainSel) {
-    // 클릭 시 on클래스가 추가되며 옵션선택박스 열림
+    let mainSelList = x.querySelector(".main_select_list");
+    let selArrow = x.querySelector(".main_select_arrow span");
     x.onclick = () => {
-        x.classList.add("on");
-        selArrow.forEach(ele=>{
-            ele.style.transform = "rotate(-180deg)";
-        })
+        let selHeight = mainSelList.querySelector("ul").offsetHeight;
+        
+        // 옵션선택박스 열리기
+        mainSelList.style.height = selHeight+"px";
+        mainSelList.style.transition = "height .3s ease-out";
+        
+        // 화살표아이콘 방향변경
+        selArrow.style.transform = "rotate(-180deg)";
+        console.log(selHeight, mainSelList.offsetHeight);
+        // 한번 더 클릭 시 옵션선택박스 닫히가
+        if(mainSelList.offsetHeight > 1) {
+            mainSelList.style.height = "0";
+            selArrow.style.transform = "rotate(0)";
+        }
     }
-}
+    // 옵션선택박스에서 누른 숫자 적용
+    // 대상선정 
+    const selVal = x.querySelector(".main_select_value");
+    const selOpt = mainSelList.querySelectorAll(".option");
+    // 숫자 적용 함수
+    const chSel = function(val) {
+        selVal.innerHTML = val.innerText
+    }
 
-// 대상선정 
-const selVal = document.querySelectorAll(".main_select_value");
+    selOpt.forEach(function(option){
+        option.addEventListener("click", function(){chSel(option)})
+    }) 
+}
