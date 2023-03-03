40e1c408db479023ac5d3ea242b19f0a697666e6
DAIN302 <dbgnfk12@gmail.com>
2023-03-04 00:06:10 +0900

    팀플젝

--- 04.팀 프로젝트/03.구현소스/js/episode2.js
+++ 04.팀 프로젝트/03.구현소스/js/episode2.js
@@ -3,5 +3,18 @@
 window.addEventListener("DOMContentLoaded", loadFn);
 
 function loadFn() {
-    console.log("로딩완");
-}
+    // 1. 대상선정
+    const chaList = document.querySelector(".mcha_list ul");
+    // 2. 함수생성 : ul에 데이터 세팅
+    function characList () {
+        let hcode = "";
+        for(let x in cdata) {
+            let data = cdata[x];
+            console.log(data);
+        }
+    }
+
+    // 임시호출
+    characList();
+
+} // 로드함수
