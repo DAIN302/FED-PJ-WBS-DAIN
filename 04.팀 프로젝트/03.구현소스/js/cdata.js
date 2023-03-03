911a0ea6bae413f909f2f1c714fc63b4e6741cf0
DAIN302 <dbgnfk12@gmail.com>
2023-03-03 17:14:18 +0900

    팀프로젝트
    캐릭터 파트 구현

---
+++ 04.팀 프로젝트/03.구현소스/js/cdata.js
@@ -0,0 +1,54 @@
+// 캐릭터 객체 데이터 
+
+const cdata = {
+    "anakin" : {
+        name : "Anakin Skywalker",
+        imglink : "anakin.jpg",
+        desc : "Discovered as a slave on Tatooine by Qui-Gon Jinn and Obi-Wan Kenobi, Anakin Skywalker had the potential to become one of the most powerful Jedi ever. Some even believed he was the prophesied Chosen One who would bring balance to the Force. Always pushing the limits of his Jedi training, seeking to excel and live up to his reputation, Skywalker's passion often brought him into conflict with his mentor, Jedi Master Obi-Wan Kenobi. A hero of the Clone Wars, Anakin was caring and compassionate, but suffered from a deep fear of loss that would prove to be his downfall.",
+    },
+    "padme" : {
+        name : "Padmé Amidala",
+        imglink : "padme.jpeg",
+        desc : "Padmé Amidala was a courageous, hopeful leader, serving as Queen and then Senator of Naboo -- and was also handy with a blaster. Despite her ideals and all she did for the cause of peace, her secret, forbidden marriage to Jedi Anakin Skywalker would prove to have dire consequences for the galaxy.",
+    },
+    "obiwan" : {
+        name : "Obi-Wan Kenobi",
+        imglink : "obiwan.jpeg",
+        desc : "A legendary Jedi Master, Obi-Wan Kenobi was a noble man and gifted in the ways of the Force. He trained Anakin Skywalker, served as a general in the Republic Army during the Clone Wars, and guided Luke Skywalker as a mentor.",
+    },
+    "yoda" : {
+        name : "Yoda",
+        imglink : "yoda.jpeg",
+        desc : "Yoda was a legendary Jedi Master and stronger than most in his connection with the Force. Small in size but wise and powerful, he trained Jedi for over 800 years, playing integral roles in the Clone Wars, the instruction of Luke Skywalker, and unlocking the path to immortality.",
+    },
+    "c3po" : {
+        name : "C-3PO (See-Threepio)",
+        imglink : "c3po.jpeg",
+        desc : "C-3PO longs for more peaceful times, but his continued service to the Resistance — and his knowledge of more than seven million forms of communication — keeps the worry-prone droid in the frontlines of galactic conflict. Programmed for etiquette and protocol, Threepio was built by a young Anakin Skywalker, and has been a constant companion to astromech R2-D2. Over the years, he was involved in some of the galaxy’s most defining moments and thrilling battles. Since the Empire’s defeat, C-3PO has served Leia Organa, head of a Resistance spy ring aimed at undermining the First Order.",
+    },
+    "r2d2" : {
+        name : "R2-D2",
+        imglink : "r2d2.jpeg",
+        desc : "A reliable and versatile astromech droid, R2-D2 has served Padmé Amidala, Anakin Skywalker, and Luke Skywalker in turn, showing great bravery in rescuing his masters and their friends from many perils. A skilled starship mechanic and fighter pilot's assistant, he has an unlikely but enduring friendship with the fussy protocol droid C-3PO.",
+    },
+    "windu" : {
+        name : "Mace Windu",
+        imglink : "windu.jpeg",
+        desc : "A grim Jedi Master with an amethyst-bladed lightsaber, Mace Windu was the champion of the Jedi Order, with little tolerance for the failings of the Senate, the arguments of politicians, or the opinions of rebellious Jedi. As the Clone Wars intensified, Mace sensed the dark side of the Force at work, and knew the Jedi's enemies were plotting to destroy the Order and end its stewardship of the galaxy.",
+    },
+    "palpatine" : {
+        name : "Emperor Palpatine / Darth Sidious",
+        imglink : "palpatine.jpeg",
+        desc : "The dark side of the Force is a pathway to many abilities some consider to be unnatural, and Sheev Palpatine is the most infamous follower of its doctrines. Scheming, powerful, and evil to the core, Darth Sidious restored the Sith and destroyed the Jedi Order. Living a double life, he was also Palpatine, a Naboo Senator and phantom menace. He manipulated the political system of the Galactic Republic until he was named Supreme Chancellor -- and eventually Emperor – and ruled the galaxy through fear and tyranny. The galaxy rejoiced when he died at the Battle of Endor, but Sidious had cheated death and patiently plotted a return to power.",
+    },
+    "jango" : {
+        name : "Jango Fett",
+        imglink : "jangofett.jpeg",
+        desc : "In the final years of the Republic, Jango Fett was regarded as the best bounty hunter in the galaxy. A proficient marksman and unarmed combatant, Fett was covered in a sleek armored suit that concealed his scarred face. His combat gear featured an arsenal of weaponry, including retractable wrist blades, a snare, dual blaster pistols and other more exotic tools of the trade. In combat, Jango used his harnessed jetpack to gain the advantage of speed and height over his enemies. The backpack also carried a nasty surprise -- an explosive rocket could be launched from it. For interstellar travel, Jango traveled aboard his well-worn starship, Slave I.",
+    },
+    "dooku" : {
+        name : "Count Dooku",
+        imglink : "dooku.jpeg",
+        desc : "Count Dooku was a menacing Sith Lord and central figure in the Clone Wars. Once a Jedi -- trained by Yoda -- he became disillusioned with the Jedi Order and thirsted for greater power. Dooku voluntarily left the light side behind and became Darth Sidious’ dark side disciple, taking the secret name Darth Tyranus and leading the Separatist army. Dooku advanced Sidious’s secret plot to take over the galaxy, but forgot that betrayal is the nature of the Sith.",
+    },
+}
---
+++ 04.팀 프로젝트/03.구현소스/js/episode2.js
@@ -0,0 +1,7 @@
+// 서브페이지 js
+
+window.addEventListener("DOMContentLoaded", loadFn);
+
+function loadFn() {
+    console.log("로딩완");
+}
