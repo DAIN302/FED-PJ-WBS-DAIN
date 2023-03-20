974e38f211719f4e4496044d350d14fce52061f6
DAIN302 <dbgnfk12@gmail.com>
2023-03-21 00:07:34 +0900

    1차 플젝 서브페이지
    서브페이지

--- 01.웹 프로젝트/03.구현소스/js/contact.js
+++ 01.웹 프로젝트/03.구현소스/js/contact.js
@@ -1,5 +1,43 @@
 // 서브페이지 JS - contact.js
 
+const fdata = {
+    fait : {
+        name : "Fait Maison",
+        loca : "서울시 송파구 잠실로 209 (4층)",
+        tel : "02-2092-6100",
+        fax : "02-2092-6100",
+        mail : "HB220-FB7@SOFITEL.COM"
+    },
+    mio : {
+        name : "MIO",
+        loca : "서울시 송파구 잠실로 209 (3층)",
+        tel : "02-2092-6102",
+        fax : "02-2092-6102",
+        mail : "HB220-FB10@SOFITEL.COM"
+    },
+    jardin : {
+        name : "Jardin D'Hiver",
+        loca : "서울시 송파구 잠실로 209 (6층)",
+        tel : "02-2092-6106",
+        fax : "02-2092-6106",
+        mail : "HB220-FB8@SOFITEL.COM"
+    },
+    espace : {
+        name : "L'Espace",
+        loca : "서울시 송파구 잠실로 209 (6층)",
+        tel : "02-2092-6104",
+        fax : "02-2092-6104",
+        mail : "HB220-FB9@SOFITEL.COM"
+    },
+    latitude : {
+        name : "Latitude32",
+        loca : "서울시 송파구 잠실로 209 (32층)",
+        tel : "02-2092-6108",
+        fax : "02-2092-6108",
+        mail : "HB220-FB11@SOFITEL.COM"
+    },
+}; 
+
 $(document).ready(function() {
     // 1. 처음에 다 숨기기
     $(".sub_cont_locadesc").addClass("hidden");
