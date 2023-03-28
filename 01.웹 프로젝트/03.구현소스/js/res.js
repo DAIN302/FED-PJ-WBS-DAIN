0d43bc7825926817d7ad2893ff7a78aabb633e8a
DAIN302 <dbgnfk12@gmail.com>
2023-03-28 11:47:21 +0900

    1차 플젝
    1. 메인페이지 미디어쿼리 적용
    2. 서브페이지 미디어쿼리 적용
    (rooms, luxury, contact 완료)
    3. 서브페이지 JS 수정
    (reservation)

--- 01.웹 프로젝트/03.구현소스/js/res.js
+++ 01.웹 프로젝트/03.구현소스/js/res.js
@@ -22,8 +22,6 @@ $(document).ready(function() {
     // 변경 대상 
     // (1) .sub_cont_numcntbx 하위요소
     const resNum = x => $(".sub_cont_numcntbx").children(x);
-    // (2) .sub_cont_roomsel 
-    const roomsel = $(".sub_cont_roomsel");
     
     // 이벤트 대상
     // (1) 버튼
@@ -32,17 +30,12 @@ $(document).ready(function() {
 
     // 숫자 변수
     let cnt = 0;
-
-    // 숫자 적용 변수
-    // let roomCounts = 1;
-
     let adultNumberCheck = $(".sub_cont_numadult").find("span").find("em").text();
-
 
     // 어린이 인원 0일 시 표시 X
     resNum(".sub_cont_ccnt").hide();
 
-    // 성인 수 0일때 어린이 수 증가 X
+    // 성인 수 0일때 어린이 수 증가 X -> 버튼 비활성화
     for(let i=0; i<adultNumberCheck.length; i++) {
         console.log()
         if(Number(adultNumberCheck[i])===0) {
@@ -74,7 +67,7 @@ $(document).ready(function() {
                 cnt--;
                 if(cnt < 0) return;
             }
-            miNum.text(cnt).parent().siblings("input").attr("value", cnt);             
+            miNum.text(cnt).parent().siblings("input").val(cnt);
 
             // 어른 숫자 반영
             if(isAdult) roomInfoArray[buttonIndex - 1]["adult"] = cnt;
@@ -83,9 +76,9 @@ $(document).ready(function() {
 
             //성인이 0명 일때
             let isAdultZero = roomInfoArray[buttonIndex - 1]["adult"];
+            // 성인이 0명일 때 어린이 숫자 초기화 및 숫자 증가 비활성화
             if(isAdultZero === 0 ) {
                 cnt = 0;
-                // totalChildNumber = 0;
                 $(this).parent().siblings(".sub_cont_numchild").find(".btnup").attr("disabled", true);
                 $(this).parent().siblings(".sub_cont_numchild").find("span").find("em").text(cnt);
                 for(let i = 1; i<roomInfoArray.length; i++){
@@ -94,15 +87,27 @@ $(document).ready(function() {
             }
 
             adultNumberCheck = $(".sub_cont_numadult").find("span").find("em").text();
-            console.log(adultNumberCheck[1]);
+
+            // 객실 1의 성인 수 1명 이하로 내려가지 않게 설정 
+            if(Number(adultNumberCheck[0]) < 2 ) {
+                $(".sub_cont_numadult").first().find(".btndown").attr("disabled", true)
+            }
+
+            // 객실2의 성인 수가 0명이면 객실 3의 숫자 초기화 
             if(Number(adultNumberCheck[1])===0) {
                 $(".sub_cont_roomsel").last().find(".sub_cont_numbx")
                 .find(".sub_cont_numadult").find("span").find("em")
                 .text(0).parent().siblings(".btnup").attr("disabled", true)
                 .parent().siblings(".sub_cont_numchild").find("span").find("em").text(0);
+                $(".sub_cont_numbx").eq(1).removeClass("active");
                 if(roomInfoArray[2]["adult"]>0) {
                     roomInfoArray[2]["adult"] = 0;   
+                    $(".sub_cont_numbx").eq(2).removeClass("active");
                 }
+            }
+            // 객실 3의 성인 수가 0명이면 밑줄 흐리게
+            if(Number(adultNumberCheck[2])===0) {
+                $(".sub_cont_numbx").eq(2).removeClass("active");
             }
 
             refreshTotalInfo();
@@ -117,7 +122,7 @@ $(document).ready(function() {
             cnt = plusNum.text();
             cnt++;
             if(cnt > 5) return;
-            plusNum.text(cnt).parent().siblings("input").attr("value", cnt); 
+            plusNum.text(cnt).parent().siblings("input").val(cnt);
 
 
             // 어른 숫자 반영
@@ -127,15 +132,27 @@ $(document).ready(function() {
 
             //성인이 0명 이상인가?
             let isAdultZero = roomInfoArray[buttonIndex - 1]["adult"];
+            // 성인이 0명이상일 때 어린이 숫자 증가 버튼 활성화
             if(isAdultZero > 0 ) {
                 $(this).parent().siblings(".sub_cont_numchild").find(".btnup").attr("disabled", false);
             }
 
             adultNumberCheck = $(".sub_cont_numadult").find("span").find("em").text();
+
+            // 객실 1의 성인 수 2명 이상일 때 감소 버튼 활성화 
+            if(Number(adultNumberCheck[0]) > 1 ) {
+                $(".sub_cont_numadult").first().find(".btndown").attr("disabled", false)
+            }
+            // 객실 2의 성인이 1명 이상일 때 객실 3 버튼 활성화
             console.log(adultNumberCheck[1]);
             if(Number(adultNumberCheck[1])>0) {
                 $(".sub_cont_roomsel").last().find(".sub_cont_numbx")
-                .find(".sub_cont_numadult").find(".btnup").attr("disabled", false)
+                .find(".sub_cont_numadult").find(".btnup").attr("disabled", false);
+                $(".sub_cont_numbx").eq(1).addClass("active");
+            }
+            // 객실 3의 성인이 1명이상일 때 밑줄 진하게
+            if(Number(adultNumberCheck[2])>0) {
+                $(".sub_cont_numbx").eq(2).addClass("active");
             }
 
             refreshTotalInfo();
@@ -144,14 +161,12 @@ $(document).ready(function() {
     } // roomCount
 
 
-    // 호출 
+    // 함수 호출 
     roomCount();
 
     function GetTotalAdultNumber(){
         let totalNumber = 0;
         roomInfoArray.forEach(roominfo => {
-            // totalNumber += number;
-            // console.log(roominfo);
             totalNumber += roominfo["adult"];
         });
 
@@ -183,12 +198,9 @@ $(document).ready(function() {
 
         let roomCounts = 0;
         roomInfoArray.forEach((element, idx) => {
-            // console.log(element, idx);
             let eleAdult = element.adult;
-            // console.log(eleAdult);
             if(eleAdult > 0) {
                 roomCounts = idx+1;
-                //console.log(roomCounts);
             }
         });
         
