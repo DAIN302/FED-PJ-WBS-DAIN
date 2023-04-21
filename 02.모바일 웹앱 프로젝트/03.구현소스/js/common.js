1b973601f3da1aaa64c1c8c347326dffd8cd5fe7
DAIN302 <dbgnfk12@gmail.com>
2023-04-21 17:13:29 +0900

    2차 프로젝트
    gnb 메뉴

---
+++ 02.모바일 웹앱 프로젝트/03.구현소스/js/common.js
@@ -0,0 +1,33 @@
+// 공통 JS
+$(()=>{
+    // 서브메뉴 등장 -> shop 글씨 클릭했을때만
+    $(".gnb>ul>li>a").first().click(function(e){
+        e.preventDefault();
+        $(".sidebar_menu").addClass("show");
+    })
+
+
+
+    // 서브메뉴
+    // 1. 처음 다 숨기기
+    $(".sidebar_submenu").addClass("hidden");
+    // 2. 클릭이벤트 설정
+    $(".sub_menu>li>h2").click(function(){
+        // 2-0. 다른 h2 요소 클릭 시 다른 메뉴는 접히기
+        $(".sub_menu>li>h2").not(this).removeClass("active")
+        .next().slideUp(400, function(){
+            $(this).addClass("hidden").show();
+        });
+        // 2-1. h2요소 active 클래스 존재 시(하위메뉴 접기)
+        if($(this).hasClass("active")) {
+            $(this).removeClass("active")
+            .next().slideUp(400, function(){
+                $(this).addClass("hidden").show();
+            });
+        }
+        // 2-2. h2요소에 active 클래스 없을 시(하위메뉴 펼치기)
+        else {
+            $(this).addClass("active").next().removeClass("hidden").hide().slideDown(400);
+        }
+    })// click
+})
