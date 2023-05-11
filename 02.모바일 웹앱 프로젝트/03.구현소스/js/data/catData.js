bf4db42dcd90ddf6ee69c2ea9122fbf7c51e616e
DAIN302 <dbgnfk12@gmail.com>
2023-05-11 17:15:34 +0900

    Update catData.js
    데이터 세팅

--- 02.모바일 웹앱 프로젝트/03.구현소스/js/data/catData.js
+++ 02.모바일 웹앱 프로젝트/03.구현소스/js/data/catData.js
@@ -8,8 +8,166 @@ const perfumeListData = {
         volume : ["50ml", "100ml"],
         price : [147250, 204250],
         gender : {
-            men : ["formal", "elegant"]
+            men : ["formal", "elegant"],
+            women : []
         },
         notes : ["woody", "citrus"]
     }, 
+    mirto : {
+        namekor : "미르토 디 파나레아 오 드 뚜왈렛",
+        nameeng : "Mirto Di Panarea Eau de Toilette",
+        brand : "ACQUA DI PARMA",
+        image : "mirto_30",
+        volume : ["30ml", "75ml", "150ml"],
+        price : [104500, 161500, 247500],
+        gender : {
+            men : ["street"],
+            women : []
+        },
+        notes : ["citrus"]
+    }, 
+    osman : {
+        namekor : "오스만투스 오 드 퍼퓸",
+        nameeng : "Osmanthus Eau de Parfum",
+        brand : "ACQUA DI PARMA",
+        image : "osman_20",
+        volume : ["20ml", "100ml"],
+        price : [147250, 332500],
+        gender : {
+            mem : [],
+            women : ["lovely"],
+        },
+        notes : ["floral","citrus"]
+    }, 
+    pico : {
+        namekor : "피코 디 아말피 오 드 뚜왈렛",
+        nameeng : "Pico Di Amalfi Eau de Toilette",
+        brand : "ACQUA DI PARMA",
+        image : "pico_30",
+        volume : ["30ml", "75ml", "150ml"],
+        price : [104500, 161500, 247500],
+        gender : {
+            men : ["street"],
+            women : []
+        },
+        notes : ["woody"]
+    }, 
+    rosa : {
+        namekor : "로사 노빌레 오 드 퍼퓸",
+        nameeng : "Rosa Nobile Eau de Parfum",
+        brand : "ACQUA DI PARMA",
+        image : "rosa_20",
+        volume : ["20ml", "50ml", "100ml"],
+        price : [118750, 213750, 299250],
+        gender : {
+            men : [],
+            women : ["lovely"]
+        },
+        notes : ["floral"]
+    }, 
+    yuzu : {
+        namekor : "유자 오 드 퍼퓸",
+        nameeng : "Yuzu Eau de Parfum",
+        brand : "ACQUA DI PARMA",
+        image : "yuzu_20",
+        volume : ["20ml", "100ml"],
+        price : [147250, 332500],
+        gender : {
+            mem : [],
+            women : ["lovely"],
+        },
+        notes : ["floral","citrus"]
+    }, 
+    bal : {
+        namekor : "발다프리크 오 드 퍼퓸",
+        nameeng : "Bal d'Afrique Eau de Parfum",
+        brand : "BYREDO",
+        image : "bal_50",
+        volume : ["50ml", "100ml"],
+        price : [215000, 305000],
+        gender : {
+            mem : ["formal"],
+            women : ["modern"],
+        },
+        notes : ["floral","citrus"]
+    }, 
+    blan : {
+        namekor : "블랑쉬 오 드 퍼퓸",
+        nameeng : "Blanche Eau de Parfum",
+        brand : "BYREDO",
+        image : "blan_50",
+        volume : ["50ml", "100ml"],
+        price : [215000, 305000],
+        gender : {
+            mem : ["street"],
+            women : ["chic"],
+        },
+        notes : ["floral"]
+    }, 
+    ghost : {
+        namekor : "모하비고스트 오 드 퍼퓸",
+        nameeng : "Mojave Ghost Eau de Parfum",
+        brand : "BYREDO",
+        image : "ghost_50",
+        volume : ["50ml", "100ml"],
+        price : [215000, 305000],
+        gender : {
+            mem : ["elegant"],
+            women : ["chic", "modern"],
+        },
+        notes : ["floral", "woody"]
+    }, 
+    inf : {
+        namekor : "인플로레센스 오 드 퍼퓸",
+        nameeng : "Inflorescence Eau de Parfum",
+        brand : "BYREDO",
+        image : "inf_50",
+        volume : ["50ml", "100ml"],
+        price : [215000, 305000],
+        gender : {
+            mem : [],
+            women : ["lovely"],
+        },
+        notes : ["floral"]
+    }, 
+    lat : {
+        namekor : "라튤립 오 드 퍼퓸",
+        nameeng : "La Tulipe Eau de Parfum",
+        brand : "BYREDO",
+        image : "lat_50",
+        volume : ["50ml", "100ml"],
+        price : [215000, 305000],
+        gender : {
+            mem : [],
+            women : ["lovely"],
+        },
+        notes : ["floral"]
+    }, 
+    sc : {
+        namekor : "슈퍼시더 오 드 퍼퓸",
+        nameeng : "Super Cedar Eau de Parfum",
+        brand : "BYREDO",
+        image : "sc_50",
+        volume : ["50ml", "100ml"],
+        price : [215000, 305000],
+        gender : {
+            mem : ["formal"],
+            women : [],
+        },
+        notes : ["woody"]
+    }, 
+    pardoson : {
+        namekor : "도손 오 드 퍼퓸",
+        nameeng : "Doson Eau de Parfum",
+        brand : "BYREDO",
+        image : "par-doson_75",
+        volume : ["75ml"],
+        price : [214800],
+        gender : {
+            mem : [],
+            women : ["lovely"],
+        },
+        notes : ["floral"]
+    }, 
+
 }
