f4ff2ab6b29b3e3cfb3143229aa4771ed7ec6973
DAIN302 <dbgnfk12@gmail.com>
2023-06-15 23:48:52 +0900

    3차 플젝

--- 03.SPA 프로젝트/03.구현소스/dain-app/src/game/modules/Story.js
+++ 03.SPA 프로젝트/03.구현소스/dain-app/src/game/modules/Story.js
@@ -3,6 +3,29 @@ import $ from "jquery";
 import "../css/story.css"
 
 function Story(){
+    const charater_img = [
+        {
+            isrc : "./images/story/yellowCookie.webp",
+            name : "vanilla",
+        },
+        {
+            isrc : "./images/story/pinkCookie.webp",
+            name : "hollyberry",
+        },
+        {
+            isrc : "./images/story/purpleCookie.webp",
+            name : "cacao",
+        },
+        {
+            isrc : "./images/story/whiteCookie.webp",
+            name : "lily",
+        },
+        {
+            isrc : "./images/story/orangeCookie.webp",
+            name : "cheese",
+        },
+    ]
+    
     return(
         <>
             <section className="story_sec">
@@ -12,12 +35,20 @@ function Story(){
                 </div>
                 <div className="story_cont">
                     <div className="story_desc"></div>
-                    <div className="story_img"></div>
+                    <div className="story_img">
+                        {
+                            charater_img.map((v,i)=>
+                                <figure key={i} id={v.name+"_img"}>
+                                    <img src={v.isrc} alt={v.name}/>
+                                </figure>
+                            )
+                        }
+                    </div>
                     <div className="story_bgimg">
-                        <figure>
+                        <figure className="cloud_img" id="backCloud">
                             <img src="./images/story/backCloud.webp" alt="뒷구름"/>
                         </figure>
-                        <figure>
+                        <figure className="cloud_img" id="frontCloud">
                             <img src="./images/story/frontCloud.webp" alt="앞구름"/>
                         </figure>
                     </div>
